d8122cdad1be847f2c7726f8058fadad1e7a0557
Rogero Carvalho <rogerintel@gmail.com>
2022-11-21 23:40:44 +0000

    habilitando lancamento deexceptions para propriedades desconhecidas

--- src/main/java/com/impacto/algafood/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/impacto/algafood/api/exceptionHandler/ApiExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.impacto.algafood.api.exceptionHandler;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.impacto.algafood.domain.exception.EntidadeEmUsoException;
 import com.impacto.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.impacto.algafood.domain.exception.NegocioException;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +16,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.stream.Collectors;
+
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
 
+        Throwable rootCause = ExceptionUtils.getRootCause(ex);
+
+        if (rootCause instanceof InvalidFormatException) {
+            return handleInvalidFormatException((InvalidFormatException) rootCause, headers, status, request);
+        }
         PloblemType ploblemType = PloblemType.MENSAGEM_INCOMPREENSIVEL;
         String detail = "O corpo da reuisição está inválido. Verifique erro de sintaxe";
 
@@ -27,8 +38,26 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         return super.handleExceptionInternal(ex, problem, headers, status, request);
     }
 
+    private ResponseEntity<Object> handleInvalidFormatException(InvalidFormatException rootCause, HttpHeaders headers,
+                                                                HttpStatus status, WebRequest request) {
+
+        String path = rootCause.getPath().stream()
+                .map(JsonMappingException.Reference::getFieldName)
+                .collect(Collectors.joining("."));
+
+        PloblemType ploblemType = PloblemType.MENSAGEM_INCOMPREENSIVEL;
+        String detail = String.format("A propriedade '%s' recebeu o valor '%s', que é de um tipo inválido. " +
+                "Corrija e informe um valor compatível com o tipo %s.", path, rootCause.getValue(),
+                rootCause.getTargetType().getSimpleName());
+
+        Problem problem = createProblemBuilder(status, ploblemType, detail).build();
+
+        return super.handleExceptionInternal(rootCause, problem, headers, status, request);
+    }
+
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<Object> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e, WebRequest request) {
+    public ResponseEntity<Object> handleEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e,
+                                                                       WebRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         PloblemType problemType = PloblemType.ENTIDADE_NAO_ENCONTRADA;
         String detail = e.getMessage();
@@ -71,7 +100,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, @Nullable Object body, HttpHeaders headers,
-            HttpStatus status, WebRequest request) {
+                                                             HttpStatus status, WebRequest request) {
         if (body == null) {
             body = Problem.builder()
                     .title(status.getReasonPhrase())
