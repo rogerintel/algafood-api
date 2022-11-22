488d945fe8fc88bdaae3965bedcf61852cb32b69
Rogero Carvalho <rogerintel@gmail.com>
2022-11-22 02:59:50 +0000

    estendendo o problem details para adicionar as propriedades

--- src/main/java/com/impacto/algafood/api/exceptionHandler/Problem.java
+++ src/main/java/com/impacto/algafood/api/exceptionHandler/Problem.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @Builder
@@ -14,5 +16,14 @@ public class Problem {
     private String title;
     private String detail;
     private String userMessage;
+    private List<Field> fields;
+
+    @Getter
+    @Builder
+    public static class Field {
+
+        private String name;
+        private String userMessage;
+    }
 
 }
