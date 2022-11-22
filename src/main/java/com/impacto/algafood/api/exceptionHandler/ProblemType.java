382b53a54e7af36eae34205d2f035bd9a251f14b
Rogero Carvalho <rogerintel@gmail.com>
2022-11-22 02:52:52 +0000

    tratando exception de violacao de constraints de validacao

--- src/main/java/com/impacto/algafood/api/exceptionHandler/ProblemType.java
+++ src/main/java/com/impacto/algafood/api/exceptionHandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+    DADOS_INVALIDOS("Dados inválidos", "/dados-invalidos"),
     ERRO_DE_SISTEMA("Erro de sistema", "/erro-de-sistema"),
     PARAMETRO_INVALIDO("Parâmetro inválido", "/parametro-invalido"),
     MENSAGEM_INCOMPREENSIVEL("Mensagem incopreensivel", "/mensagem-incopreensivel"),
