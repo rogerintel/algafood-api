73587ca51d66ba1c369a906c2137625add003d90
Rogero Carvalho <rogerintel@gmail.com>
2022-11-22 01:35:46 +0000

    tratando a excecao nohandlerfoundexception

--- src/main/java/com/impacto/algafood/api/exceptionHandler/ProblemType.java
+++ src/main/java/com/impacto/algafood/api/exceptionHandler/ProblemType.java
@@ -7,7 +7,7 @@ public enum ProblemType {
 
     PARAMETRO_INVALIDO("Parâmetro inválido", "/parametro-invalido"),
     MENSAGEM_INCOMPREENSIVEL("Mensagem incopreensivel", "/mensagem-incopreensivel"),
-    ENTIDADE_NAO_ENCONTRADA("Entidade não encontrada", "/entidade-nao-encontrada"),
+    RECURSO_NAO_ENCONTRADO("Entidade não encontrada", "/entidade-nao-encontrada"),
     ERRO_NEGOCIO("Erro de negócio", "/erro-negocio"),
     ENTIDADE_EM_USO("Entidade em uso", "/entidade-em-uso");
 
