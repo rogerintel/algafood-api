c7558590f16ea96bc9b2fff074ae97fdc3fe0a77
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2022-12-04 11:34:57 -0300

    escrevendo bons nomes de testes

--- src/test/java/com/impacto/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/impacto/algafood/CadastroCozinhaIntegrationTests.java
@@ -3,17 +3,14 @@ package com.impacto.algafood;
 import com.impacto.algafood.domain.model.Cozinha;
 import com.impacto.algafood.domain.service.CadastroCozinhaService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.validation.ConstraintViolationException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class CadastroCozinhaIntegrationTests {
 
@@ -21,7 +18,7 @@ public class CadastroCozinhaIntegrationTests {
     private CadastroCozinhaService cadastroCozinha;
 
     @Test
-    public void testarCadastroCozinhaComSucesso() {
+    public void deveAtribuirId_QuandoCadastrarCozinhaComDadosCorretos() {
         Cozinha novaCozinha = new Cozinha();
         novaCozinha.setNome("Chinesa");
         novaCozinha = cadastroCozinha.salvar(novaCozinha);
