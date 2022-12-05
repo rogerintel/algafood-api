1c6e481c04650d67e296ed457333fb9d6e6c49d6
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2022-12-05 16:10:25 -0300

    configurando maven failsafe plugin no projeto

--- src/test/java/com/impacto/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/impacto/algafood/CadastroCozinhaIT.java
@@ -14,7 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-public class CadastroCozinhaIntegrationTests {
+public class CadastroCozinhaIT {
 
     @Autowired
     private CadastroCozinhaService cadastroCozinha;
