aa4d4424a6e0e3e675984cf1e35dfdb51c800045
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2022-12-06 10:27:53 -0300

    analisando e definindo melhor o escopo das transacoes

--- src/main/java/com/impacto/algafood/domain/service/CadastroCozinhaService.java
+++ src/main/java/com/impacto/algafood/domain/service/CadastroCozinhaService.java
@@ -8,19 +8,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CadastroCozinhaService {
 
-    public static final String MSG_COZINHA_NAO_ENCONTRADA = "Não existe um cadastro de cozinha com código %d";
     public static final String MSG_COZINHA_EM_USO = "Cozinha de código %d não pode ser removida, pois está em uso";
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @Transactional
     public Cozinha salvar(Cozinha cozinha) {
         return cozinhaRepository.save(cozinha);
     }
 
+    @Transactional
     public void excluir(Long cozinhaId) {
         try {
             cozinhaRepository.deleteById(cozinhaId);
