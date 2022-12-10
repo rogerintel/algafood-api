2d2db38f9f7b022ef476862b7664d35de52542ac
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2022-12-10 20:09:12 -0300

    adicionando endereco no modelo da representacao do recurso de restaurante

--- src/main/java/com/impacto/algafood/core/modelMapper/ModelMapperConfig.java
+++ src/main/java/com/impacto/algafood/core/modelMapper/ModelMapperConfig.java
@@ -1,7 +1,7 @@
 package com.impacto.algafood.core.modelMapper;
 
-import com.impacto.algafood.api.model.RestauranteModel;
-import com.impacto.algafood.domain.model.Restaurante;
+import com.impacto.algafood.api.model.EnderecoModel;
+import com.impacto.algafood.domain.model.Endereco;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +13,14 @@ public class ModelMapperConfig {
     public ModelMapper modelMapper() {
         var modelMapper = new ModelMapper();
 
-        modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
-            .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+        var enderecoToEnderecoModelTypeMap = modelMapper.createTypeMap(
+                Endereco.class,
+                EnderecoModel.class);
+
+        enderecoToEnderecoModelTypeMap.<String>addMapping(
+                enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(),
+                (enderecoModelDest, value) -> enderecoModelDest.getCidade().setEstado(value));
+
         return modelMapper;
     }
 }
